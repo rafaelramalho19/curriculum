fb6696604d69d8e52c30fb3a228f7c8cb4a2f4c8
Rafael Ramalho <rafael.ramalho@blip.pt>
2019-07-30 18:30:30 +0100

    Add stylelint and bootstrap script files

---
+++ js/companies.js
@@ -0,0 +1,19 @@
+const companies = [
+  {
+    name: "Blip",
+    startDate: new Date(2018, 0),
+    endDate: null
+  },
+  {
+    name: "Pixelmatters",
+    startDate: new Date(2016, 11),
+    endDate: new Date(2017, 11)
+  },
+  {
+    name: "FYI",
+    startDate: new Date(2016, 3),
+    endDate: new Date(2016, 10)
+  }
+];
+
+export { companies };
---
+++ js/cover.js
@@ -0,0 +1,3 @@
+import { companies } from "./companies";
+
+console.log(companies);
